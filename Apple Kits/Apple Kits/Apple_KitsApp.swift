7420818a190e877467968e282c46acb0b6bc0ba1
Viral Parmar <viralparmarme@gmail.com>
2023-10-21 09:03:15 +0530

    Initial setup commit

---
+++ Apple Kits/Apple Kits/Apple_KitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_KitsApp.swift
+//  Apple Kits
+//
+//  Created by Viral Parmar on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_KitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Kits/Apple Kits/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple Kits
+//
+//  Created by Viral Parmar on 21/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
